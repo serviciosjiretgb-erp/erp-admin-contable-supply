dfac1180d5e0631035058bd67ffdeb6ff649f034
serviciosjiretgb-erp <serviciosjiretgb@gmail.com>
2026-04-10 20:27:08 -0400

    Add Firebase configuration
    Added Firebase configuration to main.jsx.

--- main.jsx
+++ main.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const firebaseConfig = {
+  apiKey: "AIzaSyD4fIG6D94cuwHA0ZC-diHBwWHs8woMbkc",
+  authDomain: "erp-admin-contable-supply.firebaseapp.com",
+  projectId: "erp-admin-contable-supply",
+  storageBucket: "erp-admin-contable-supply.firebasestorage.app",
+  messagingSenderId: "138985315880",
+  appId: "1:138985315880:web:534e8335f763589ac91f1d",
+  measurementId: "G-6N7D0QVYW7"
+};
